fc2331efb3114146dd06b7c9f42bd34659e8e110
Pat <Patgiok@gmail.com>
2022-03-22 22:29:42 -0700

    added torus

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+// perspective cam mimics human eye
+// first param is field of view
+// 2nd param is aspect ratio; base off user browser window
+// 3rd 4th param is view frustrum
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// uses dom element
+const renderer = new THREE.WebGL1Renderer({
+  canvas:document.querySelector('#bg'),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight);
+
+camera.position.setZ(30);
+
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100);
+// basic material doesnt need light source
+const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe:true} );
+// make mesh combining geometry and material
+const torus = new THREE.Mesh( geometry, material);
+
+scene.add(torus)
+
